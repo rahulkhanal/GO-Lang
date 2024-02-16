2fac58418157b13a3b56cec8e555317ef29582df
Rahul Khanal <rahul39khanal@gmail.com>
2024-02-16 22:19:55 +0545

    go repo intitalization

---
+++ app.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	Don()
+	var x int = 9
+	fmt.Println("%d", x)
+}
---
+++ hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Don() {
+	fmt.Println("I am outside")
+}
